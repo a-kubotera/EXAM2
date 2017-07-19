daa2cd446cdac107c5c8798294805e0ceb6a77cc
a-kubotera <akira.kubotera+github@gmail.com>
2017-07-19 21:49:04 +0900

    Heroku対応

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,9 +1,27 @@
 module PicturesHelper
-  def getdata_method
-    if action_name == 'new'
-      confirm_tweets_path
-    else action_name == 'edit'
-      tweet_path
+  def users_avatar(user)
+    @avatar = user.avatar
+    if @avatar.blank?
+      @avatar = "no_image.png"
     end
+      return image_tag(@avatar, alt: user.name,class:"users_avater")
+  end
+
+  def users_name(user)
+    @name = user.name
+    if @name.blank?
+      @name = "名無しさん"
+    end
+      return @name
+  end
+
+  def your_picture(picture)
+    @picture = picture.image.url
+    @name = picture.name
+    if @picture.blank?
+      @picture = erased.png
+      @name = "Herokuに消されてるよ！"
+    end
+      return image_tag(@picture, alt: @name)
   end
 end
